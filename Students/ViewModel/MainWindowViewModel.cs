384caeae23229edb9a6ec6d0419d65c842290aec
AndreyXP <andreyapple99@mail.ru>
2023-09-26 16:02:18 +0300

    Вынос шаблонов в славари

--- Students/ViewModel/MainWindowViewModel.cs
+++ Students/ViewModel/MainWindowViewModel.cs
@@ -8,70 +8,62 @@ using Students.Models.Decanat;
 
 namespace Students.ViewModel;
 
-    [ObservableObject]
-    internal partial class MainWindowViewModel
+[ObservableObject]
+internal partial class MainWindowViewModel
+{
+    [ObservableProperty] private ObservableCollection<Group> _groups;
+
+    [ObservableProperty] private Group _selectedGroup;
+
+    [ObservableProperty] private List<object> _compositeCollection;
+
+    [ObservableProperty] private object _selectedCompositeValue;
+
+    public MainWindowViewModel()
     {
-        [ObservableProperty] private ObservableCollection<Group> _groups;
-
-        [ObservableProperty] private Group _selectedGroup;
-
-        [ObservableProperty] private List<object> _compositeCollection;
-
-        [ObservableProperty] private object _selectedCompositeValue;
-
-        public MainWindowViewModel()
+        var student_index = 1;
+        var student = Enumerable.Range(1, 10).Select(i => new Student
         {
-            var student_index = 1;
-            var student = Enumerable.Range(1, 10).Select(i => new Student
-            {
-                Name = $"Name {student_index}",
-                Surname = $"Surname {student_index}",
-                Patronymic = $"Patronymic {student_index}",
-                Birthday = DateTime.Now,
-                Rating = 0,
-                Description = $"Description {student_index++}"
-                
-            });
+            Name = $"Name {student_index}",
+            Surname = $"Surname {student_index}",
+            Patronymic = $"Patronymic {student_index}",
+            Birthday = DateTime.Now,
+            Rating = 0,
+            Description = $"Description {student_index++}"
 
-            var group = Enumerable.Range(1, 20).Select(i => new Group
-            {
-                Name = $"Группа {i}",
-                Students = new ObservableCollection<Student>(student),
-            });
+        });
 
-            Groups = new ObservableCollection<Group>(group);
-
-            CompositeCollection = new() { "Hello Word", 42, Groups[1], group, };
-        }
-
-
-
-
-
-
-
-
-
-        [RelayCommand]
-        private void onCreateGroup()
+        var group = Enumerable.Range(1, 20).Select(i => new Group
         {
-            var max_group = Groups.Count + 1;
-            var new_group = new Group
-            {
-                Name = $"Группа {max_group}",
-                Students = new ObservableCollection<Student>()
-            };
+            Name = $"Группа {i}",
+            Students = new ObservableCollection<Student>(student),
+        });
 
-            Groups.Add(new_group);
-        }
+        Groups = new ObservableCollection<Group>(group);
 
-        [RelayCommand]
-        private void onDeleteGroup()
+        CompositeCollection = new() { "Hello Word", 42, Groups[1], group, };
+    }
+
+    [RelayCommand]
+    private void onCreateGroup()
+    {
+        var max_group = Groups.Count + 1;
+        var new_group = new Group
         {
-            if (Groups.Contains(_selectedGroup))
-            {
-                Groups.Remove(_selectedGroup);
-            }
+            Name = $"Группа {max_group}",
+            Students = new ObservableCollection<Student>()
+        };
+
+        Groups.Add(new_group);
+    }
+
+    [RelayCommand]
+    private void onDeleteGroup()
+    {
+        if (Groups.Contains(_selectedGroup))
+        {
+            Groups.Remove(_selectedGroup);
         }
     }
+}
 
