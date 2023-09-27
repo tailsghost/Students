519855a633ac75ee6d88434ac5474a2feaa44fcf
AndreyXP <andreyapple99@mail.ru>
2023-09-27 04:47:32 +0300

    Фильтрация

--- Students/Views/Windows/MainWindow.xaml.cs
+++ Students/Views/Windows/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using Students.Models.Decanat;
 
 namespace Students.Views.Windows
 {
@@ -10,6 +14,28 @@ namespace Students.Views.Windows
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void GroupsCollection_OnFilter(object sender, FilterEventArgs e)
+        {
+            if (!(e.Item is Group group)) return;
+            if (group.Name is null) return;
+
+            var filter_text = GroupNameFilterText.Text;
+            if(filter_text.Length == 0) return;
+
+            if (group.Name.Contains(filter_text, StringComparison.OrdinalIgnoreCase)) return;
+            if (group.Description != null && group.Description.Contains(filter_text, StringComparison.OrdinalIgnoreCase)) return;
+
+            e.Accepted = false;
+        }
+
+        private void onGroupFilterText(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            var text_box = (TextBox)sender;
+            var collection = (CollectionViewSource)text_box.FindResource("GroupsCollection");
+
+            collection.View.Refresh();
         }
     }
 }
